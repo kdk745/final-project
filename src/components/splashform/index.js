a8293d87d7ad5b4ea88a78fd4924fa2f3f0b816d
Kayne Khoury <kaynekhoury1@gmail.com>
2017-03-05 21:47:48 -0600

    added material-ui pgk

---
+++ src/components/splashform/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextField from 'material-ui/TextField';
+
+const splashform = () => {
+  <div>
+    <TextField
+      hintText="Search with Address, Zip Code, or City"
+    />
+  </div>
+}
+
+export default splashform
