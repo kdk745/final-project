3c5427a06eb6945841bb895bb59fac47a0362d8d
Kayne Khoury <kaynekhoury1@gmail.com>
2017-03-07 23:01:25 -0600

    scaled splash form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import splashform from './components/splashform';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Splashform from './components/Splashform';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <splashform/>
+        <MuiThemeProvider>
+          <Splashform
+            hint="search by zip code or city"
+          />
+        </MuiThemeProvider>
       </div>
     );
   }
--- src/components/splashform/index.js
+++ src/components/splashform/index.js
@@ -1,12 +1,66 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import TextField from 'material-ui/TextField';
+import './index.css';
+import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
 
-const splashform = () => {
-  <div>
-    <TextField
-      hintText="Search with Address, Zip Code, or City"
-    />
-  </div>
+class Splashform extends Component {
+  state = {
+    value: 1,
+    partySelect: [1,2,3,4,5,6,7],
+    partySize: 1,
+  };
+
+
+  handleTextChange = (val) => {
+    this.setState({value: val});
+  };
+
+  handleTblChange = (val) => {
+    console.log(this.state.partySize);
+    this.setState({partySize: val});
+  };
+
+  render() {
+    return (
+      <div className="splash-container">
+        <div className="splash-section">
+          <div className="splash-row">
+            <AppBar
+              className="app-bar"
+              title="Welcome"
+              showMenuIconButton={false}
+            />
+            <TextField
+              className="splash-text-field"
+              floatingLabelText="Search for Restaurants"
+              hintText={this.props.hint}
+              onChange={(event) => this.handleTextChange(event.target.value)}
+            />
+          </div>
+          <div className="splash-row">
+            <span>Party Size</span>
+            <select
+              onChange={(event) => this.handleTblChange(event.target.value)}
+            >
+              {this.state.partySelect.map(val => {
+                return (
+                  <option key={val} value={val}>{val}</option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="splash-row">
+            <RaisedButton label="Submit" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default splashform
+Splashform.propTypes = {
+  hint: PropTypes.string
+};
+
+export default Splashform;
